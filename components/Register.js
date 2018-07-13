13b6986df25f0f632ed4c9fe774e2ef3900407f9
ckc1373 <ccourtne@hamilton.edu>
2018-07-13 11:47:04 -0700

    added stack-navigator, split into three screens/files

---
+++ components/Register.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import mainStyles from '../styles.js'
+
+
+export default class Register extends React.Component {
+  render() {
+    return (
+      <View style={mainStyles.container}>
+        <Text>Register</Text>
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+
+});
