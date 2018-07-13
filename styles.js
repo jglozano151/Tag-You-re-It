4aae0de8236bb65b2af743ebbaf9c7edab3e0219
ckc1373 <ccourtne@hamilton.edu>
2018-07-13 12:09:55 -0700

    Global styles

--- styles.js
+++ styles.js
@@ -10,6 +10,51 @@ const mainStyles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius: 5
+  },
+  blue: {
+    backgroundColor: '#4286f4'
+  },
+  black: {
+    backgroundColor: '#000000'
+  },
+  lightGrey: {
+    backgroundColor: '#b1bed8'
+  },
+  darkGrey: {
+    backgroundColor: '#848a96'
+  },
+  buttonLabel: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'white'
+  },
+  textBig: {
+    fontFamily: 'Baskerville',
+    fontSize: 36,
+    textAlign: 'center',
+    margin: 10,
+  },
+  textMed: {
+    fontFamily: 'Baskerville',
+    fontSize: 24,
+    textAlign: 'center',
+    margin: 10,
+  },
+  textSmall: {
+    fontFamily: 'Baskerville',
+    fontSize: 12,
+    textAlign: 'center',
+    margin: 10,
+  },
 });
 
 export default mainStyles
